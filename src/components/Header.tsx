9acdc8cfc71acb76efad838cad4719c4267b3657
FranckHeCai <senlouboi.egame@gmail.com>
2025-08-09 22:45:43 +0200

    logo path changed to src

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <div>
-      <img className="w-50 h-auto absolute top-5 left-1/2 -translate-x-1/2" src="/logo.png" alt="typesprint logo" />
+      <img className="w-50 h-auto absolute top-5 left-1/2 -translate-x-1/2" src="logo.png" alt="typesprint logo" />
     </div>
   );
 };
